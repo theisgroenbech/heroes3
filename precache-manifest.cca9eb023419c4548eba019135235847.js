067d47387b5c90200d405ab976222e0a7a68c1be
Theis Groenbech Petersen <tgp@siteimprove.com>
2020-03-22 22:52:25 +0100

    Updates

---
+++ precache-manifest.cca9eb023419c4548eba019135235847.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54547953b9f3a5ba43de0ff8216f4bf4",
+    "url": "/heroes3/index.html"
+  },
+  {
+    "revision": "0ce02ecd673d06443d06",
+    "url": "/heroes3/static/css/main.86a38700.chunk.css"
+  },
+  {
+    "revision": "05d7de14880af7fd9167",
+    "url": "/heroes3/static/js/2.e82db1cb.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/heroes3/static/js/2.e82db1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ce02ecd673d06443d06",
+    "url": "/heroes3/static/js/main.17a14360.chunk.js"
+  },
+  {
+    "revision": "13495a2f4dd4b498540f",
+    "url": "/heroes3/static/js/runtime-main.2276724f.js"
+  },
+  {
+    "revision": "c4ec3c8f2cce66512ded50208b7a1120",
+    "url": "/heroes3/static/media/logo.c4ec3c8f.png"
+  }
+]);
